b05f933d26260dd7b16d642678dd514cd88ec09f
ikrad <ibrahim.krad@nrel.gov>
2015-08-31 09:09:33 -0600

    Type Definitions
    Github Test

---
+++ Type_Definitions.m
@@ -0,0 +1,29 @@
+%%%%%%%                       TYPES                              %%%%%%%
+             
+disp(' ');
+disp('Generator Types');
+disp('1  - Steam generator');
+disp('2  - Combustion turbine');
+disp('3  - Combined cycle');
+disp('4  - Hydro generation');
+disp('5  - Nuclear generation');
+disp('6  - Pumped storage hydro');
+disp('7  - Wind generation');
+disp('8  - Compressed air energy storage');
+disp('9  - Limited energy storage resource');
+disp('10 - Photovoltaic');
+disp('11 - Concentrating solar plant');
+disp('12 - ');
+disp('13 - ');
+disp('14 - Interface or interchange schedule');
+disp('15 - Unit outage');
+disp('16 - Variable dispatch unit');
+disp(' ');
+
+disp('Transmission Types');
+disp('1 - AC line');
+disp('2 - Fixed phase angle regulator');
+disp('3 - Adjustable phase angle regulator');
+disp('4 - HVDC');
+
+
